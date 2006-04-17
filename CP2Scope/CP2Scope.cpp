747effeb59680057328ea01acd5bbdac07fed256
milan <milan@e5e72e4b-7c09-0410-958a-85c34e28b278>
2006-04-17 20:11:29 +0000

    Add functionality to assemble display data from one gate over sequential pulses: "along the beam". Add pulsenumber out-of-sequence error detection and display. Additions to support piraqx header access. Fix display-update bug.

--- CP2Scope/CP2Scope.cpp
+++ CP2Scope/CP2Scope.cpp
@@ -5,10 +5,12 @@
 #include <qlabel.h>
 #include <qtimer.h>
 #include <qspinbox.h>	
+#include <qlcdnumber.h>
 
 #include <Knob/Knob.h>
 
 #include <winsock.h>
+//#include <winsock2.h>
 #include <iostream>
 
 CP2Scope::CP2Scope():
@@ -17,8 +19,8 @@ m_pDataSocketNotifier(0),
 m_pSocketBuf(0),	
 _plotType(ScopePlot::TIMESERIES)
 {
-	this->m_pSpinBoxPort->setMinValue(WINDSP_BASE_PORT);
-	this->m_pSpinBoxPort->setMaxValue(WINDSP_BASE_PORT + 3);
+	this->m_pSpinBoxPort->setMinValue(QTDSP_BASE_PORT);
+	this->m_pSpinBoxPort->setMaxValue(QTDSP_BASE_PORT + 3);
 	m_dataGramPort	= this->m_pSpinBoxPort->value();
 	m_packetCount	= 0; 
 	// create the socket that receives packets from WinDSP  
@@ -26,7 +28,7 @@ _plotType(ScopePlot::TIMESERIES)
 	connectDataRcv(); 
 
 	// Start a timer to tell the display to update
-	m_dataDisplayTimer = startTimer(DISPLAY_REFRESH_INTERVAL);
+	m_dataDisplayTimer = startTimer(CP2SCOPE_DISPLAY_REFRESH_INTERVAL);
 	//connect( ... do with QTimer later see Notes
 
 	//	set knob assuming TIMESERIES: 
@@ -45,10 +47,15 @@ _plotType(ScopePlot::TIMESERIES)
 				 FFTW_FORWARD, FFTW_ESTIMATE);
 	//	power correction factor applied to (uncorrected) powerSpectrum() output:
 	_powerCorrection = 0.0;	//	use for power correction to dBm
+	//	make the LCD more visible: 
+	_PNerrors->setSegmentStyle(QLCDNumber::Flat);
 
-	I.resize(500);			//	moved to constructor
-	Q.resize(500);
+	I.resize(1000);			//	default timeseries array size full range at 1KHz
+	Q.resize(1000);
 
+	_parametersSet = 0;		//	piraqx parameters not yet initialized from received data
+	_dataSet = DATA_SET_PULSE;
+m_DataSetGate = 50;		//!get spinner value
 }
 
 CP2Scope::~CP2Scope() {
@@ -65,61 +72,90 @@ CP2Scope::~CP2Scope() {
 
 void 
 CP2Scope::timerEvent(QTimerEvent *e) {
-	displayData();	//	this works here
+	int	i;
+	int	hit;	//	hit in packet to display
+
+	if	(m_packetCount == 0)	//	no data
+		return;
+	hit	= (pow(2,16)/(sizeof(PACKETHEADER) + 8*_gates)) - 1;	//	data set PULSE: display LAST pulse in this packet
+	float* p = ((float*)(m_pSocketBuf)) + _pulseStride*hit + (sizeof(PACKETHEADER)/sizeof(float));	//	
+
+	if	(_dataSet == DATA_SET_PULSE)	{	//	data set pulse: assemble once per timer event
+		for (int i = 0; i < 2*_gates; i+=2) {	
+#ifdef SCALE_DATA	//	PIRAQ data: scale to +/- 1.0 full scale PIRAQ3D_SCALE
+			I[i/2] = p[i]*PIRAQ3D_SCALE;
+			Q[i/2] = p[i+1]*PIRAQ3D_SCALE;
+#else
+			I[i/2] = p[i];
+			Q[i/2] = p[i+1];
+#endif
+		}
+		displayData();	
+	}
+//only if PULSE:	displayData();	
 }
 
 void 
 CP2Scope::plotTypeSlot(bool b)
 {
-  switch (buttonGroup->selectedId()) {
-      case 0:
-		  _plotType = ScopePlot::TIMESERIES;
+	switch (buttonGroup->selectedId()) {
+		case 0:
+			_plotType = ScopePlot::TIMESERIES;
 			//	set knob for TIMESERIES: 
 			this->yScaleKnob->setTitle("Ampl. Scale"); 
-//	m_display_yScale = ySCALEMAX / 10.0;	// start off reasonable
-//	m_yScaleMin = ySCALEMIN;	// set initial y-scale min, max
-//	m_yScaleMax	= ySCALEMAX;	
-	m_display_yScale = m_yScaleMax / 10.0;	// start off reasonable
-
-	this->yScaleKnob->setRange(m_yScaleMin, m_yScaleMax); 
-		  break;
-      case 1:
-		  _plotType = ScopePlot::IVSQ;
+			m_display_yScale = m_yScaleMax / 10.0;	// start off reasonable
+			this->yScaleKnob->setRange(m_yScaleMin, m_yScaleMax); 
+			break;
+		case 1:
+			_plotType = ScopePlot::IVSQ;
 			this->yScaleKnob->setTitle("Ampl. Scale"); 
-         break;
-      case 2:
-		  _plotType = ScopePlot::SPECTRUM;
+			break;
+		case 2:
+			_plotType = ScopePlot::SPECTRUM;
 			//	set knob for Power correction: 
 			this->yScaleKnob->setTitle("Power Corr"); 
-//			m_display_yScale = 0.0;	// start off reasonable
-//			m_yScaleMin = -30.0;	// set initial y-scale min, max
-//			m_yScaleMax	= 30.0;	
-//			this->yScaleKnob->setRange(m_yScaleMin, m_yScaleMax); 
 			this->yScaleKnob->setRange(-30.0, 30.0); 
-
-         break;
-      default: {}
+			break;
+		default: {}
    }
 
 }
 
+void 
+CP2Scope::dataSetSlot(bool b)	{
+	switch (buttonGroup2->selectedId()) {
+		case 0:
+			_dataSet = DATA_SET_PULSE;
+			I.resize(m_fft_blockSize);	//	resize timeseries arrays, x-axis
+			Q.resize(m_fft_blockSize);
+			//	disable gate spinner
+			break;
+		case 1:
+			_dataSet = DATA_SET_GATE;
+			I.resize(_gates);	//	resize timeseries arrays, x-axis
+			Q.resize(_gates);
+			//	enable gate spinner
+			break;
+		default: {}
+	}
+}
+
 void CP2Scope::yScaleKnob_valueChanged( double yScaleKnobSetting)	{	
-//		std::cout << "y-Scale Knob setting " << yScaleKnobSetting << std::endl;
 	switch (_plotType) {
 		case 0:	//	timeseries displays
 		case 1:
-		if (yScaleKnobSetting == 0.0)	{	//	y-scale set to minimum
-			m_yScaleMax *= 0.1;				//	re-scale /10
-			m_display_yScale = m_yScaleMax / 10.0; 
-			yScaleKnob->setRange(m_yScaleMin, m_yScaleMax);	//
-		}
-		else if (yScaleKnobSetting == m_yScaleMax)	{	//	max'd y-scale
-			m_yScaleMax *= 10.0;	//	re-scale *10
-			yScaleKnob->setRange(m_yScaleMin, m_yScaleMax);
-		}
-		else
-			m_display_yScale = yScaleKnobSetting; 
-		break;
+			if (yScaleKnobSetting == 0.0)	{	//	y-scale set to minimum
+				m_yScaleMax *= 0.1;				//	re-scale /10
+				m_display_yScale = m_yScaleMax / 10.0; 
+				yScaleKnob->setRange(m_yScaleMin, m_yScaleMax);	//
+			}
+			else if (yScaleKnobSetting == m_yScaleMax)	{	//	max'd y-scale
+				m_yScaleMax *= 10.0;	//	re-scale *10
+				yScaleKnob->setRange(m_yScaleMin, m_yScaleMax);
+			}
+			else
+				m_display_yScale = yScaleKnobSetting; 
+			break;
 		case 2:	//	power display
 			_powerCorrection = yScaleKnobSetting; 
 			break;
@@ -130,10 +166,17 @@ void CP2Scope::DatagramPortSpinBox_valueChanged( int datagramPort )	{
 	//	change the input data port
 	m_dataGramPort    = datagramPort;
 	terminateSocket();
-	m_packetCount = 0;		//	clear packet count 
+	m_packetCount = 0;		//	clear packet count
+	_parametersSet = 0;		//	request get parameters for this data channel
+
 	_packetCount->setNum(m_packetCount);	// update display  
 	initializeSocket();
 	connectDataRcv(); 
+}
+
+void CP2Scope::DataSetGateSpinBox_valueChanged( int SpinBoxGate )	{
+	//	change the gate for assembling data sets 
+	m_DataSetGate    = SpinBoxGate;
 }
 
 
@@ -143,21 +186,86 @@ CP2Scope::connectDataRcv()	// connect notifier to data-receive slot
 	connect(m_pDataSocketNotifier, SIGNAL(activated(int)), this, SLOT(dataSocketActivatedSlot(int)));
 }
 
+int
+CP2Scope::getParameters( CP2_PIRAQ_DATA_TYPE[] )	// get program operating parameters from piraqx (or other) "housekeeping" header structure 
+{
+	_gates = *(int *)((m_pSocketBuf) + 108/sizeof(int));	//	gates: 32+28+48 as expected; 
+	_pulseStride = (sizeof(PACKETHEADER) + 8*_gates)/sizeof(float);	//	stride in floats; 2nd term DATASIZE: 8 bytes per gate wired for now 
+	//	compute #hits combined by piraq: equal in both piraq executable (CP2_DCCS3_1.out) and host applications
+	_Nhits = UDPSENDSIZE / (HEADERSIZE + (8*_gates));	//	8 bytes per gate: define
+	return(1);	//	condition this 
+}
+
 void 
 CP2Scope::dataSocketActivatedSlot(int socket)
 {
-	static int count = 0;
 	static int errcount = 0;
-
-	int readBufLen, lastreadBufLen;
+	static int lasterrcount = 0;
+	static int readBufLen, lastreadBufLen;
+	static int j = 0;	//	Data Set Gate Idx; move to constructor to make accessible to datagram port change to reset
+	int hit = 0;		//	index into N-hit packet
+	int	r_c;	//	return_code
+	//	define p: pointer to packet data
+	float* p = ((float*)(m_pSocketBuf)) + _pulseStride*hit + (sizeof(PACKETHEADER)/sizeof(float));	//	
+	uint8* PNptr = (uint8*)((char *)(m_pSocketBuf) + 32 + 28 + 32);	//	
+	static	uint8	PN, lastPN;
+	uint4	gates, lastgates; 
 
 	m_packetCount++; 
 	readBufLen = m_pDataSocket->readBlock((char *)m_pSocketBuf, sizeof(short)*1000000);
+	if	(readBufLen != lastreadBufLen)	{	//	packet size changed: hmmmmm .... could be all right if CP2exec stop/start so generalize to cover this. 
+//		errcount++;  
+	}
 	lastreadBufLen = readBufLen;
+		if	((errcount % 100) == 10)	//	currently counting out-of-sequence PNs
+			r_c = 5;	//	
 
-	if (!(count++ % 50))	{
+	if	(!_parametersSet)	{	//	piraqx parameters need to be initialized from received data
+		r_c = getParameters( m_pSocketBuf );	//	 
+		if	(r_c == 1)	{		//	success
+			if	(_dataSet == DATA_SET_PULSE)	{	//	data set along pulse
+				I.resize(_gates);	//	resize timeseries arrays, x-axis
+				Q.resize(_gates);
+			}
+			else	{	//if	(_dataSet == DATA_SET_GATE)	{	//	data set along beam: 1 gate per pulse
+				I.resize(m_fft_blockSize);	//	resize timeseries arrays, x-axis
+				Q.resize(m_fft_blockSize);
+			}
+			_parametersSet = 1; 
+		}
+	}
+	//	NOT DONE! gather Nhits data per notification; deal w/partial sets ... ? could drop them continuity w/in set is what is required. 
+	if	(_dataSet == DATA_SET_GATE)	{	//	gather data along one gate ("along the beam")#ifdef SCALE_DATA
+		//	set p to 1st pulse in packet
+		for	(hit = 0; hit < _Nhits; hit++)	{	//	all hits in packet
+			PN	= *PNptr;
+#ifdef	SCALE_DATA
+			I[j/2] = p[2*m_DataSetGate]*PIRAQ3D_SCALE;	//	get selected gate data
+			Q[j/2] = p[(2*m_DataSetGate)+1]*PIRAQ3D_SCALE;
+#else
+			I[j/2] = p[2*m_DataSetGate];
+			Q[j/2] = p[2*m_DataSetGate+1];
+#endif
+			//	maintain p, detect full set, break
+			p += _pulseStride; 
+			j += 2; 
+			if	(PN != lastPN + 1)	{	
+				errcount++;	
+				_PNerrors->display(errcount);	//	display accumulated PN errors
+			}	//	we'll always get 1 
+			lastPN = PN; 
+			PNptr = (uint8 *)((char *)PNptr + _pulseStride*sizeof(float)); 
+			if	(j >= m_fft_blockSize*2)	{	//	full set 
+				if	(lasterrcount == errcount)	{	//	no missed pulses
+					displayData(); 
+				}
+				j = 0; 
+				lasterrcount = errcount; 
+			}
+		}
+	}
+	if (!(m_packetCount % 50))	{
 		if(readBufLen > 0) {
-//			std::cout << "read returned " << readBufLen << std::endl;
 			_packetCount->setNum(m_packetCount);	// update cumulative packet count 
 		}
 	}
@@ -167,28 +275,15 @@ CP2Scope::dataSocketActivatedSlot(int socket)
 void
 CP2Scope::displayData() 
 {
-	// display data -- activated by QTimer timeout()
-//	std::vector<double> I;	//	moved to constructor
-//	std::vector<double> Q;
-//	I.resize(500);			//	moved to constructor
-//	Q.resize(500);
-//	I.resize(200);	//	easy zoom from start of data
-//	Q.resize(200);
+	// display data -- activated by QTimer timeout() if pulse set, or fft size assembled if gate set. 
 
-	float* p = ((float*)(m_pSocketBuf)) + 764/sizeof(float);	//	hardwire offset to first datum: 704+32+28
-//double p0, p1, p2, p3, p04;// p5; 
+//float p0, p1, p2, p3, p04;// p5; 
 //p126, p127, p128, p129, p130, p131, p132, p254, p255; 
 //p0 = p[0]; p1 = p[1]; p2 = p[2]; p3 = p[3]; p04 = p[4];// p5 = p[5]; 
 //p126 = p[126]; p127 = p[127]; 
 //p128 = p[128]; p129 = p[129]; 
 //p130 = p[130]; p131 = p[131]; p132 = p[132]; 
-//p254 = p[254]; p255 = p[255];  
-	for (int i = 0; i <1000; i+=2) {	
-//	for (int i = 0; i <400; i+=2) {	//	easy zoom from start of data	
-		//	PIRAQ data:
-		I[i/2] = p[i];
-		Q[i/2] = p[i+1];
-	} 
+//p254 = p[254]; p255 = p[255];
 
 	switch (_plotType) {
 		case ScopePlot::TIMESERIES:
@@ -209,10 +304,8 @@ CP2Scope::displayData()
 			for(unsigned int j = 0; j < m_fft_blockSize; j++)	{
 				_spectrum[j] += _powerCorrection;
 			}
-//			_scopePlot->Spectrum(_spectrum, 1.0e-70, 1.0e+10, 1, "Frequency (Hz)", "Power (dB)"); 
-//_scopePlot->Spectrum(_spectrum, -1.0e-04, 1.0e+04, 1000000, "Frequency (Hz)", "Power (dB)"); 
-			//	test w/linear limits
-_scopePlot->Spectrum(_spectrum, -100, 20.0, 1000000, "Frequency (Hz)", "Power (dB)"); 
+			//	use Spectrum() w/linear limits:
+			_scopePlot->Spectrum(_spectrum, -100, 20.0, 1000000, "Frequency (Hz)", "Power (dB)"); 
 			break;
 	}
 }
