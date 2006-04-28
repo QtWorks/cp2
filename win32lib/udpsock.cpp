158fad53e75ed54f8b58d4be79410014d4c2bf09
milan <milan@e5e72e4b-7c09-0410-958a-85c34e28b278>
2006-04-28 16:58:10 +0000

    fix datagram send bug. now have CP2 data integrity through CP2exec, QtDSP, CP2Scope; no missing pulsenumbers or data.

--- win32lib/udpsock.cpp
+++ win32lib/udpsock.cpp
@@ -1,4 +1,5 @@
 #include <Afx.h>
+//#include <stdafx.h>
 
 #include <errno.h>
 #include <signal.h>
@@ -7,6 +8,7 @@
 #include <string.h>
 //#include <winsock.h>
 #include <winsock2.h>
+//#include <Ws2spi.h>
 #include <sys/types.h>
 
 #include	"../include/proto.h"
@@ -25,7 +27,76 @@ int	wastime(int in)  {return in;}
 
  // this is the guy who usually transmits 
  // this is the server 
-int open_udp_out(char *name)
+#ifdef	STREAM_SOCKET
+void open_udp_out(char *name)	//	Stream implementation
+   {
+  // Initialize Winsock
+  WSADATA wsaData;
+  int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+  if (iResult != NO_ERROR)
+    printf("Error at WSAStartup()\n");
+
+  //----------------------
+  // Create a SOCKET for connecting to server
+  SOCKET ConnectSocket;
+  ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+  if (ConnectSocket == INVALID_SOCKET) {
+    printf("Error at socket(): %ld\n", WSAGetLastError());
+    WSACleanup();
+    return;
+  }
+
+  //----------------------
+  // The sockaddr_in structure specifies the address family,
+  // IP address, and port of the server to be connected to.
+  sockaddr_in clientService; 
+  clientService.sin_family = AF_INET;
+  clientService.sin_addr.s_addr = inet_addr( "128.117.85.189" );
+  clientService.sin_port = htons( 27015 );
+
+  //----------------------
+  // Connect to server. 
+  if ( connect( ConnectSocket, (SOCKADDR*) &clientService, sizeof(clientService) ) == SOCKET_ERROR) {
+    printf( "Failed to connect. sizeof(clientService) = %d\n", sizeof(clientService) );
+int iError = WSAGetLastError ();
+if (iError) { 
+	printf("connect(): iError = 0x%x\n",iError);
+}
+    WSACleanup();
+    return;
+  }
+
+  //----------------------
+  // Declare and initialize variables.
+  int bytesSent;
+  int bytesRecv = 0;
+  char sendbuf[32] = "Client: Sending data.";
+  char recvbuf[32] = "";
+
+  //----------------------
+  // Send and receive data.
+  bytesSent = send( ConnectSocket, sendbuf, strlen(sendbuf), 0 );
+  printf( "Bytes Sent: %ld\n", bytesSent );
+
+  while( bytesRecv != SOCKET_ERROR ) {
+    bytesRecv = recv( ConnectSocket, recvbuf, 32, 0 );
+    if ( bytesRecv == 0 ) {
+      printf( "Connection Closed.\n");
+      break;
+    }
+    printf( "Bytes Recv: %ld\n", bytesRecv );
+  }
+
+  if ( bytesRecv == SOCKET_ERROR )
+  {
+     printf("recv failed: %d\n", WSAGetLastError());
+  }
+
+  WSACleanup();
+  return;
+   }
+#else	//	UDP implementation: previous method
+int open_udp_out(char *name)	//	Datagram implementation
    {
 	int	sock,i,error;
 	struct hostent* hent;
@@ -42,20 +113,27 @@ int open_udp_out(char *name)
 
 		if (error)
 		{ // there was an error
+			printf("!!!WSAStartup(): error = -1\n");
 		  return -1;
 		}
 		if (w.wVersion != 0x0202)
 		{ // wrong WinSock version!
 		  WSACleanup (); // unload ws2_32.dll
+			printf("!!!WSACleanup(): error = -1\n");
 		  return -1;
 		}
       }
 
-    // set up the socket stuff 
+    // set up the socket stuff  
    memset(&SockAddr, 0, sizeof(struct sockaddr_in));
 
    //if((sock = socket(AF_INET, SOCK_DGRAM, 0)) == ERROR)  return(-1);  // get a UDP socket 
-   sock = WSASocket(AF_INET,SOCK_DGRAM,IPPROTO_UDP,NULL,0,0);
+   sock = WSASocket(AF_INET,SOCK_DGRAM,IPPROTO_UDP,NULL,0,WSA_FLAG_OVERLAPPED);	//	was dwFlags = 0
+int iError;
+if (1) { 
+	iError = WSAGetLastError ();
+	printf("!!!WSASocket(): iError = 0x%x\n",iError);
+}
 	//int bufLen = 1024*1024*2;
 	//int size = 4;
 	//setsockopt(sock,SOL_SOCKET,SO_SNDBUF,"100000",strlen("100000"));
@@ -70,6 +148,7 @@ int open_udp_out(char *name)
 
    return(sock);
    }
+#endif
    
 //???static unsigned int sequence_num = 0;  
 
@@ -94,80 +173,28 @@ unsigned int send_udp_packet(int sock, int port, unsigned int sequence_num, UDPH
    WSAOVERLAPPED Overlapped;
    DWORD dwFlags;
    DWORD lBytesSent = 0;
-//   WSABUF DataBufs[50]; // why?
-//   ZeroMemory(DataBufs,sizeof(WSABUF)*50);
    int iBuf=0;
 
-   WSABUF DataBuf;
-   DataBuf.buf = (char *)udp;
-   DataBuf.len = udp->totalsize+sizeof(UDPHEADER); // add sizeof(UDPHEADER) 4-15-05, mp
-   int iError = WSASendTo(sock,&DataBuf,1,&lBytesSent,0,(sockaddr *)&SockAddr,sizeof(SockAddr),NULL,NULL);
-#if 0
-char buf[255];
-if (iError != 0) { 
-sprintf(buf,"Error:%d, Bytes: %d",iError,lBytesSent);
-sprintf(buf,"totalsize:%d",udp->totalsize);
-MessageBox(NULL,buf,NULL,MB_OK);
-}  
+   WSABUF DataBuf[1];
+   DataBuf[0].buf = (char *)udp;
+   DataBuf[0].len = udp->totalsize; // remove +sizeof(UDPHEADER) 4-19-06, mp
+   int iError = WSASendTo(sock,&DataBuf[0],1,&lBytesSent,0,(sockaddr *)&SockAddr,sizeof(SockAddr),NULL,NULL);
+#if 1
+//  if ((sequence_num % 100) == 0) {
+   char buf[255];
+   if (iError != 0) { // send error, print it
+      printf("Error:%d, Bytes: %d ",iError,lBytesSent);
+      printf("totalsize:%d\n",udp->totalsize);
+      if (iError) { 
+	     iError = WSAGetLastError ();
+	     printf("WSASendTo(): iError = 0x%x\n",iError);
+      }
+   }
+//  }
 #endif
    sequence_num+=udp->pages;
    return(sequence_num);
 
-   for(remain=udp->totalsize; remain>0; remain-=size)
-      {
-      // send the first page 
-      size = (remain > MAXPACKET) ? MAXPACKET : remain; // data, NOT INCLUDING the header 
-      udp->pagesize = size + sizeof(UDPHEADER);
-	  udp->sequence_num = sequence_num; sequence_num++; 
-	  
-	  char *buf = new char[MAXPACKET + sizeof(UDPHEADER)];
-	  memcpy(buf,udp,MAXPACKET + sizeof(UDPHEADER));
-//not used:	  DataBufs[iBuf].len = udp->pagesize;
-//	  DataBufs[iBuf].buf = buf;//(char *)udp;
- 	  iBuf++;
-
-	       //r_c = sendto(sock,(const char *)udp, udp->pagesize,0,(struct sockaddr *)&SockAddr,sizeof(SockAddr));
-//	  if (r_c != udp->pagesize/* && (WSAGetLastError() == WSAENOBUFS)*/) {
-//			Sleep(1000); //printf(""); 
-//           r_c = sendto(sock,(const char *)udp, udp->pagesize,0,(struct sockaddr *)&SockAddr,sizeof(SockAddr));
-//	  }	  
-//#if 0
-//	  while (r_c == SOCKET_ERROR/* && (WSAGetLastError() == WSAENOBUFS)*/) {
-//			Sleep(1000); //printf(""); 
-//            r_c = sendto(sock,(const char *)udp, udp->pagesize,0,(struct sockaddr *)&SockAddr,sizeof(SockAddr));
-//	  }	  
-//#endif
-	  //Sleep(0);
-	  //usleep(1);
-//printf("sending page %d   size = %d  pagesize = %d remain = %d\n",udp->pagenum,size,udp->pagesize,remain);
-
-//for(i=0; i<1000; i++) wastime(i);
-#if 1
-      if(remain > size)
-         {
-      // fill in the next header (destroys contents of buffer!) 
-      next = (UDPHEADER *)((char *)udp + size);	// point to where the next header starts 
-      next->magic = udp->magic;
-      next->type = udp->type;
-	  next->totalsize = udp->totalsize;
-      next->pagenum = udp->pagenum+1;
-      next->pages = udp->pages;
-      udp = next;
-         }
-#endif
-      }
-		
-   	  lBytesSent = 0;
-	  ZeroMemory(&Overlapped,sizeof(WSAOVERLAPPED));
-	  Overlapped.hEvent = NULL;
-	  //WSASendTo(sock,DataBufs,iBuf,&lBytesSent,0,(struct sockaddr *)&SockAddr,sizeof(SockAddr),NULL,NULL);
-//not used:		for(int i=0;i<iBuf;i++)
-//			delete(DataBufs[i].buf);
-      
-	  //if(lBytesSent != udp->pagesize)
-	//	  MessageBox(NULL,"BUNGLE!",NULL,MB_OK);
-
-   return(sequence_num); 
    }
    
  //returns a 1 when the last page arrives 
