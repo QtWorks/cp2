8a603ec255c0f3855678ebe30b1e7a65147cb099
martinc <martinc@e5e72e4b-7c09-0410-958a-85c34e28b278>
2006-12-10 23:20:55 +0000

    Read it and weep.

---
+++ Moments/Dummy.cpp
@@ -0,0 +1,8 @@
+// 
+// This file is here simply to have a file with
+// a .cpp extension. Visual studio will not enable
+// the C++ property section in the project
+// when there isn't a file in the project with
+// this extension! Thus with the project loaded 
+// up with only .cc files, it didn't know that
+// C++ was needed.
