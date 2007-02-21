10955e0f69ad5667d4ee0ae64adea39c28fa90d4
martinc <martinc@e5e72e4b-7c09-0410-958a-85c34e28b278>
2007-02-21 23:39:40 +0000

    Removed commented out code.

--- CP2Scope/CP2Scope.cpp
+++ CP2Scope/CP2Scope.cpp
@@ -435,14 +435,12 @@ CP2Scope::initSockets()
 		QMessageBox e;
 		e.warning(this, "Error",_pPulseSocket->errorMsg().c_str(), 
 			QMessageBox::Ok, QMessageBox::NoButton);
-//		return;
 	}
 	_pProductSocket = new CP2UdpSocket(interfaceIP, _productPort, false, 0, 10000000);
 	if (!_pProductSocket->ok()) {
 		QMessageBox e;
 		e.warning(this, "Error",_pProductSocket->errorMsg().c_str(), 
 			QMessageBox::Ok, QMessageBox::NoButton);
-//		return;
 	}
 
 	m_pTextIPaddress->setText(_pPulseSocket->toString().c_str());
