a812166ea893b3f86b8b98038943cccccd824e92
martinc <martinc@e5e72e4b-7c09-0410-958a-85c34e28b278>
2007-04-12 16:34:23 +0000

    Updaing moments calculations to include some required parameters, and define radar configurations. These additions are not yet used in the code. Also removed the ABP option, which seemed identical to the PP option.

--- Moments/Params.hh
+++ Moments/Params.hh
@@ -24,97 +24,132 @@ class Params {
 
 public:
 
-  // enum typedefs
+	// enum typedefs
 
-  typedef enum debug_t {
-    DEBUG_OFF = 0,
-    DEBUG_NORM = 1,
-    DEBUG_VERBOSE = 2,
-    DEBUG_EXTRA_VERBOSE = 3
-  } debug_t;
+	typedef enum radar_config_t {
+		/// Single polarization (NEXRAD)
+		SP = 0, 
+		/// Dual pol, alternating transmission, copolar receiver only
+		/// (CP2 SBand)
+		DP_ALT_HV_CO_ONLY = 1, 
+		/// Dual pol, alternating transmission, co-polar and cross-polar
+		///receivers (SPOL with Mitch Switch and receiver in 
+		/// switching mode, CHILL)
+		DP_ALT_HV_CO_CROSS = 2,
+		/// Dual pol, alternating transmission, fixed H and V receivers (SPOL
+		/// with Mitch Switch and receivers in fixed mode)
+		DP_ALT_HV_FIXED_HV = 3,
+		/// Dual pol, simultaneous transmission, fixed H and V receivers (NEXRAD
+		/// upgrade, SPOL with T and receivers in fixed mode)
+		DP_SIM_HV_FIXED_HV = 4,
+		/// Dual pol, simultaneous transmission, switching H and V receivers
+		/// (SPOL with T and receivers in switching mode)
+		DP_SIM_HV_SWITCHED_HV = 5,
+		/// Dual pol, H transmission, fixed H and V receivers (CP2 X band)
+		DP_H_ONLY_FIXED_HV = 6
+	} radar_config_t;
 
-  typedef enum algorithm_t {
-    ALG_PP = 0,
-    ALG_ABP = 1,
-    ALG_FFT = 2
-  } algorithm_t;
+	typedef enum debug_t {
+		DEBUG_OFF = 0,
+		DEBUG_NORM = 1,
+		DEBUG_VERBOSE = 2,
+		DEBUG_EXTRA_VERBOSE = 3
+	} debug_t;
 
-  typedef enum fft_window_t {
-    WINDOW_HANNING = 0,
-    WINDOW_BLACKMAN = 1,
-    WINDOW_NONE = 2
-  } fft_window_t;
+	typedef enum algorithm_t {
+		ALG_PP = 0,
+		ALG_FFT = 1
+	} algorithm_t;
 
-  typedef enum moments_mode_t {
-    SINGLE_POL = 0,
-    DUAL_FAST_ALT = 1,
-    DUAL_CP2_XBAND = 2
-  } moments_mode_t;
+	typedef enum fft_window_t {
+		WINDOW_HANNING = 0,
+		WINDOW_BLACKMAN = 1,
+		WINDOW_NONE = 2
+	} fft_window_t;
 
-  // struct typedefs
+	typedef enum moments_mode_t {
+		SINGLE_POL = 0,
+		DUAL_FAST_ALT = 1,
+		DUAL_CP2_XBAND = 2
+	} moments_mode_t;
 
-  typedef struct radar_params_t {
-    double horiz_beam_width;
-    double vert_beam_width;
-    double pulse_width;
-    double wavelength_cm;
-    double xmit_peak_pwr;
-  } radar_params_t;
+	typedef enum moments_corr_t {
+		R0R1 = 0,
+		R1R2 = 1
+	} moments_corr_t;
 
-  typedef struct receiver_t {
-    double noise_dBm; // as digitized, i.e. after gain applied
-    double gain;
-    double radar_constant;
-    double dbz0;
-  } receiver_t;
+	// struct typedefs
 
-  typedef struct moments_params_t {
-    int n_samples;
-    double start_range;
-    double gate_spacing;
-    algorithm_t algorithm;
-    fft_window_t window;
-    moments_mode_t mode;
-  } moments_params_t;
+	typedef struct radar_params_t {
+		radar_config_t radar_config;
+		double horiz_beam_width;
+		double vert_beam_width;
+		double pulse_width;
+		double wavelength_cm;
+		double xmit_peak_pwr;
+	} radar_params_t;
 
-  ///////////////////////////
-  // Member functions
-  //
+	typedef struct receiver_t {
+		double noise_dBm; // as digitized, i.e. after gain applied
+		double noise_h_dBm;
+		double noise_v_dBm;
+		double gain;
+		double radar_constant;
+		double dbz0;
+		double system_phidp_deg;
+	} receiver_t;
 
-  ////////////////////////////////////////////
-  // Default constructor
-  //
+	typedef struct moments_params_t {
+		moments_mode_t  mode;
+		int             n_samples;
+		double          start_range;
+		double          gate_spacing;
+		algorithm_t     algorithm;
+		fft_window_t    window;
+		moments_corr_t  moments_corr_type;
+		bool            apply_clutter_filter;
+		double          azimuth_resolution;
+		bool            index_beams_in_azimuth;
+	} moments_params_t;
 
-  Params();
+	///////////////////////////
+	// Member functions
+	//
 
-  ////////////////////////////////////////////
-  // Destructor
-  //
+	////////////////////////////////////////////
+	// Default constructor
+	//
 
-  ~Params ();
+	Params();
 
-  ///////////////////////////
-  // Data Members
-  //
+	////////////////////////////////////////////
+	// Destructor
+	//
 
-  debug_t debug;
+	~Params ();
 
-  double atmos_attenuation;
-  double dbz_calib_correction;
-  double zdr_correction;
-  double ldr_correction;
+	///////////////////////////
+	// Data Members
+	//
 
-  bool index_beams_in_azimuth;
-  double azimuth_resolution;
+	debug_t debug;
 
-  radar_params_t radar;
+	double atmos_attenuation;
+	double dbz_calib_correction;
+	double zdr_correction;
+	double ldr_correction;
 
-  receiver_t hc_receiver;
-  receiver_t hx_receiver;
-  receiver_t vc_receiver;
-  receiver_t vx_receiver;
+	bool index_beams_in_azimuth;
+	double azimuth_resolution;
 
-  moments_params_t moments_params;  
+	radar_params_t radar;
+
+	receiver_t hc_receiver;
+	receiver_t hx_receiver;
+	receiver_t vc_receiver;
+	receiver_t vx_receiver;
+
+	moments_params_t moments_params;  
 
 protected:
 	void setDefault();
