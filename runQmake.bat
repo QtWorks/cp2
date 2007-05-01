123a45ba343b55b7945434849f3329a50f9076b7
martinc <martinc@e5e72e4b-7c09-0410-958a-85c34e28b278>
2007-05-01 22:39:17 +0000

    Run qmake in all sub directories.

---
+++ runQmake.bat
@@ -0,0 +1,28 @@
+REM
+REM Run qmake for QtToolbox
+REM
+
+cd CP2exec
+qmake
+cd ..
+
+cd CP2Moments
+qmake
+cd ..
+
+cd CP2Config
+qmake
+cd ..
+
+cd CP2Net
+qmake
+cd ..
+
+cd CP2Scope
+qmake
+cd ..
+
+cd CP2PPI
+qmake
+cd ..
+
